9926556f737f329426d9d19e504d677a81db4471
Vanshika310 <136240773+Vanshika310@users.noreply.github.com>
2023-07-18 22:09:02 +0530

    Simple.sol

--- Simple.sol
+++ Simple.sol
@@ -1,12 +1,27 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.7;
+pragma solidity ^0.8.0;
 
-contract Simple1 {
-    
-    string public myLord = "Kahna";
+contract Permission {
+    mapping(address => bool) private permissions;
 
-    string public MyLord = "Radha Krishna";
+    event PermissionSet(address indexed account, bool hasPermission);
 
+    constructor() {
+        permissions[msg.sender] = true; // Set contract deployer's address as initially having permission
+    }
+
+    modifier onlyOwner() {
+        require(permissions[msg.sender], "Only the contract owner can set permissions");
+        _;
+    }
+
+    function setPermissionForAddress(address account, bool hasPermission) external onlyOwner {
+        permissions[account] = hasPermission;
+        emit PermissionSet(account, hasPermission);
+    }
+
+    function checkPermissionForAddress(address account) public view returns (bool) {
+        return permissions[account];
+    }
 }
-
